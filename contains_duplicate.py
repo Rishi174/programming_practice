7faf81c155f50e368794a079431c59b926be0b07
Rishi <rushya.kambhampati@gmail.com>
2026-09-08 08:44:36 +0530

    Main Branch

--- contains_duplicate.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def hasDuplicate(self, nums: List[int]) -> bool:
-        dict_ = {}
-        for i in nums:
-            if i in dict_:
-                return True
-            dict_[i] = 1
-        return False
